09c173837575bb91047920d6b7653db3df87d9e4
GODSPE1 <godspy18@gmail.com>
2023-11-29 20:24:35 -0800

    made executable and proper indentation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-    num = ord(i)
-    if num >= 97 and num <= 122:
+        num = ord(i)
+        if num >= 97 and num <= 122:
         num = num - 32
-    num = chr(num)
+
+        num = chr(num)
         print("{}".format(num), end='')
-        print("")
+    print("")
 
