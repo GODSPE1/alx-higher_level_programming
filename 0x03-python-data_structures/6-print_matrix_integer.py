941af942003c7bbe255c4b7f8e5cd4964a9258a4
GODSPE1 <107337599+GODSPE1@users.noreply.github.com>
2023-12-05 11:48:29 +0100

    fix str.format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if  matrix != [[]]:
         for row in matrix:
             for elem in row:
-                print("{}".format(elem), end=" "
+                print("{:d}".format(elem), end=" "
                     if elem != row[-1] else '\n')
     else:
         print()
