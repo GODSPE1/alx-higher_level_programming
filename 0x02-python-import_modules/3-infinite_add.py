44311b796a9b63f358ff3a81e00503f7d0a60fb6
GODSPE1 <godspy18@gmail.com>
2023-12-06 19:06:01 -0800

    fix error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    n = len(sys.argv)
     sum = 0
-    for j in range(1, n):
-        sum = sum + int(sys.argv[j])
-        print(sum)
+    for j in range(1, len(sys.argv)):
+        sum += int(sys.argv[j])
+        print("{:d}".format(sum))
