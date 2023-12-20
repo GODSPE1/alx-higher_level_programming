d14d990a34873f46f2141b483fba5b98a4dd1bb3
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2023-12-20 15:28:37 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
     
-'''A Square class'''
+"""
+A Square class
+"""
 
 
 class Square:
     """
     This is class square that has a private attributes
 
-    Attributes:
-        size: The side size of a square
+    - size: The side size of a square
     """
 
     
     def __init__(self, size=0):
+        """
+        Initializes the object with the specified size.
+
+        Raises:
+        - TypeError: If the size is not an integer.
+        - ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if (size < 0):
@@ -21,10 +29,32 @@ class Square:
     
 
     def area(self):
-        """returns the area square
-        Args:
-            size (int): size of a side of a square
+        """
+        Computes the area of a square
+        
         Returns:
-            None
+        the current square area
         """
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        gets the size attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the value of the private attribute
+
+        Raises:
+        - TypeError: If the size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
