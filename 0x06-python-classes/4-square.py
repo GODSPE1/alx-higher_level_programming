860696f769b08acac05d48c7828627556efde52a
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2023-12-20 05:58:25 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,14 +8,9 @@ class Square:
 
 
     def __init__(self, size=0):
+        self.__size == size
         '''Initializes the square'''
         
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if (size < 0):
-            raise ValueError('size must be >= 0')
-        self.__size = size
-    
     @property
     def size(self):
         """getter function for size
