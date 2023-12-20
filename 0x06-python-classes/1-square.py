870729bb8d086732d5a9c883257e0925702f2cfe
GODSPE1 <godspy@gmail.com>
2023-12-20 03:33:06 +0100

    define a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    class square that has attributes:
+        size
+        """
+
+    def __init__(self, size):
+            """
+            iniialization function for the square class
+            """
+            self._size = size
