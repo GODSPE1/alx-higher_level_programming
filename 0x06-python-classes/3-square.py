945db4314973ce465e6221a137a74c03df053c17
GODSPE1 <godspy@gmail.com>
2023-12-20 04:31:39 +0100

    calculating area of a square using the class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+    
+'''A Square class'''
+
+
+class Square:
+    '''this is class square that has a private attributes'''
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
