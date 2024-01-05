7f9fb94e199ddb3208ea63bb4b666b0e4c0a109d
GODSPE1 <godspy18@gmail.com>
2024-01-04 20:55:13 -0800

    add str method

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""This defines a module"""
+class Rectangle:
+    """
+    This rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        gets the width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        gets the height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Sets the height of the rectangle
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Return the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns the perimeter of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width or self.__height:
+            line = '#' * self.__width
+            rectangle = (line + '\n') * (self.__height - 1) + line
+        else:
+            return ("")
+        return (rectangle)
