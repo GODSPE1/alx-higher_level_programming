f7cf444e597c1e9a976772783f05aebef9ddf15e
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-01-11 13:57:55 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,20 +3,19 @@
 This module supplies one function that add the parameters
 """
 
-    def add_integer(a, b=98):
-        """
-        This function adds two numbers
+def add_integer(a, b=98):
+    """This function adds two numbers
 
-        Args:
-            a (int): first number
-            b (int):second number
+    Args:
+        a: first number
+        b:second number
 
-        Raises:
-            TypeError: if a or b is not an integer or float
+    Raises:
+        raise TypeError if a or b is not an integer or float
 
-        Returns:
-            returns an integer addition of two numbers
-        """
+    Returns:
+        Returns an integer addition of two numbers
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be a integer")
     if type(b) is not int and type(b) is not float:
