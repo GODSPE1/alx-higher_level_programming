f89c4ad787dd83b2cc845c497ef5cfc2ed44c4b1
GODSPE1 <godspy18@gmail.com>
2024-01-15 16:28:21 -0800

    unit test for base class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from models.base import Base
+import unittest
+
+class TestBase(unittest.TestCase):
+    '''represents a class used to test the base class'''
+
+    def test_id(self):
+        '''testing the id'''
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        '''testing the id'''
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        '''testing the id'''
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
