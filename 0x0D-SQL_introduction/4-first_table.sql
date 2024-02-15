d906be354246b3b9f86f32e61b43b170e3ecb092
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 12:36:50 +0100

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,3 @@
 -- print full description of the table first_table
 -- from the database htbn_0c_0.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
