05521880bf61e47d74a1204d11f4d7ecfce517ec
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 13:21:29 +0100

    correct mispelt words and command

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,10 @@
 -- does not fail if data exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
+
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10);
-INSERT INTO seond_table(id, name , score) VALUES(2, 'Alex', 3);
-INSERT INTO second_table(id, name, score) VALUES(3, 'Bob', 14);
-INSERT INTO second_table(id, name, score) VALUES(4, 'George', 8);
+
+INSERT INTO second_table (id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table (id, name , score) VALUES(2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES(4, 'George', 8);
