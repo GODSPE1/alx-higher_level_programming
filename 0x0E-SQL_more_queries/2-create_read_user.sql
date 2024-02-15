4046e896ab5f075205cadf0b38d039c07f4b3ffc
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 22:01:56 +0100

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 
-CREATE DATABASE IF EXIST hbtn_0d_2;
-CREATE USER IF EXISTS user_0d_2@localhost IDENTIFIED WITH user_0d_2_pwd;
+CREATE DATABASE IF NOT EXIST hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED WITH user_0d_2_pwd;
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
