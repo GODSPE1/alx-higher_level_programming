ffd62a37fcf9823c366a376294605932ab11156f
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 11:27:07 +0100

    Correct syntax for delete command in MySQL

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,5 +1,5 @@
 -- script that deletes the database hbtn_0c_0 in your MySQL server.
 -- If the database hbtn_0c_0 doesn’t exist, should not fail
-DROP DATABAES IF EXIST htbn_0c_0;
+DROP DATABASE IF NOT EXISTS htbn_0c_0;
 
 
