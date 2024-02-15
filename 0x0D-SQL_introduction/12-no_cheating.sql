45388caf802ef877116769bd95a6dfcffa01c30f
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 17:29:03 +0100

    Update 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- script that updates the score of Bob to 10 in the table second_table.
-ALTER TABLE second_table WHERE name = 'Bob' UPDATE score SET score=10;
+ALTER TABLE second_table WHERE name = 'Bob', UPDATE score SET score=10;
