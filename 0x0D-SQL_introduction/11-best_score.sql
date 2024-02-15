4139195f29de29dbc9e6d3d07a6d5dbd1a77d639
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 17:09:01 +0100

    added the right spelling and syntax

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records with a score >= 10 in the table second_table of the
 -- database hbtn_0c_0 in your MySQL server.
-SELECT score FROM second_table WHERE score > 10 OR = 10 
+SELECT score FROM second_table WHERE score >= 10;
