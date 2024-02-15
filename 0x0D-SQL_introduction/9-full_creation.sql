b9b28c3206de9f515b6f81ec6adc6503256b9b88
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-02-15 13:05:13 +0100

    add correct syntax and correct spelling. Removed unwanted **space** and **line**

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- creates a table first_table in the currnt database
 -- does not fail if data exist
-CRAETE TABLE IF NOT EXISTS first_table (id INT,
-name VARCHAR(256), scoree INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 
 INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10);
