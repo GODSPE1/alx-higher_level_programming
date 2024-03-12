9b24f95dcf820f78163fff67bf3e05b966aabd01
GODSPE1 <godspy18@gmail.com>
2024-03-13 00:34:06 +0100

    create an instance of the class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width <= 0 || height <= 0 || !Number.isInteger(width) || !Number.isInteger(height)) {
+      // Create an empty object
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      // Initialize the instance attributes width and height
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    if (this.width === undefined || this.height === undefined) {
+      console.log('Invalid dimensions');
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+}
+
+module.exports = Rectangle;
