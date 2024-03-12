d7297925893f265ba201e9ff35831d4cf94290d4
GODSPE1 <godspy18@gmail.com>
2024-03-12 05:09:07 +0100

    print 3 lines using array of string and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let counter = 0;
+
+while (counter < 3) {
+  console.log(myString[counter]);
+  counter++;
+}
