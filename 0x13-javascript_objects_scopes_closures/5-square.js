df5d75ffc69712baa9eaf95b0cd0caba47db13cb
GODSPE1 <godspy18@gmail.com>
2024-03-13 02:09:55 +0100

    create a subclass square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Rectangle = require("./4-rectangle");
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+
+  print() {
+    super.print();
+  }
+
+  double() {
+    super.double();
+  }
+}
+
+module.exports = Square;
