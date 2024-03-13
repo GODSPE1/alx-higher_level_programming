ef3dd99183fe79a6d6076077a7b387e4a89e7411
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-03-13 05:48:27 +0100

    Update

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (width, height) {
+  constructor (w, h) {
     if (width <= 0 || height <= 0 || !Number.isInteger(width) || !Number.isInteger(height)) {
-      // Create an empty object
       this.width = undefined;
       this.height = undefined;
     } else {
-      // Initialize the instance attributes width and height
-      this.width = width;
-      this.height = height;
+      this.width = w;
+      this.height = h;
     }
   }
 }
