8bd7e651c8b1f81ff414e138e2cd7a5e06026192
GODSPE1 <godspy18@gmail.com>
2024-03-13 16:36:39 +0100

    refactored

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const firsLine = 'C is fun';
-const secondLine = 'Python is cool';
-const thirdLine = 'Javascript is amazing';
-console.log(firsLine);
-console.log(secondLine);
-console.log(thirdLine);
+
+const myStr = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(myStr);
