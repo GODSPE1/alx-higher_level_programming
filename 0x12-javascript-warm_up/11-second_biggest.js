f6ff5a3901b0087c0e6b937460ddf29b5a4297f8
GODSPE1 <godspy18@gmail.com>
2024-03-13 17:59:21 +0100

    apply correct standard style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,21 +4,20 @@ const args = process.argv;
 if (args <= 2) {
   console.log(0);
 } else {
-let largest = Number.NEGATIVE_INFINITY;
-let secondLargest = Number.NEGATIVE_INFINITY;
+  let largest = Number.NEGATIVE_INFINITY;
+  let secondLargest = Number.NEGATIVE_INFINITY;
 
-for (let i = 2; i < args.length; i++) {
+  for (let i = 2; i < args.length; i++) {
     const num = parseInt(args[i]);
     if (!isNaN(num)) {
-        if (num > largest) {
-            secondLargest = largest;
-            largest = num;
-        } else if (num > secondLargest && num !== largest) {
-            secondLargest = num;
-        }
+      if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+      } else if (num > secondLargest && num !== largest) {
+        secondLargest = num;
+      }
     }
-}
+  }
 
-console.log(secondLargest !== Number.NEGATIVE_INFINITY ? secondLargest : 0);
+  console.log(secondLargest !== Number.NEGATIVE_INFINITY ? secondLargest : 0);
 }
-
