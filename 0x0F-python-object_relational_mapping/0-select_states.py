3dcb06882eff7227b596b071b58c89014ec89596
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-03-15 15:00:31 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2], port=3306, database=argv[3])
 
 cur = db.cursor()
-query = "SELECT * FROM hbtn_0e_0_usa ORDER BY states_id ASC;"
+query = "SELECT * FROM states"
 
 cur.execute(query)
 results = cur.fetchall()
