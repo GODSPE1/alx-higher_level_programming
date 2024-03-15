bcc49b984d6af7efa3813dcef6605b762a8bf58e
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-03-15 14:36:22 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,7 @@ This Module list all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(user="godspe", password="finish", database="hbtn_0e_0_usa"
-)
+db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2], port=3306, database=argv[3])
 
 cur = db.cursor()
 query = "SELECT * FROM hbtn_0e_0_usa ORDER BY states_id ASC;"
