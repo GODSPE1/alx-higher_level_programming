b46993b05da043fccb630430efcc68fb5d2e8bc6
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-03-15 14:39:56 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,10 @@ This Module list all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
+    """
+    Should not execute when imported
+    """
 db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2], port=3306, database=argv[3])
 
 cur = db.cursor()
@@ -18,5 +22,3 @@ for row in results:
 
 db.close()
 
-if __name__ == "__main__":
-    pass
