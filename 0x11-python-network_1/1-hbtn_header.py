cfbea2856ddf9ffbdac0fc5fafaa7f2a364e1aef
GODSPE1 <godspy18@gmail.com>
2024-04-01 07:19:45 -0700

    sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request
-to the URL and displays the value of the X-Request-Id
-variable found in the header of the response.
+Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    data = response.info().get('X-Request-Id')
-    print(data)
+data = urllib.request.Request(sys.argv[1])
+with urllib.request.urlopen(data) as response:
+    data_response = response.info().get('X-Request-Id')
+    print(data_response)
 
 if __name__ == '__main__':
     pass
