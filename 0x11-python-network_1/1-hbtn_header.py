07256ccd6e38d5b304504c916cd065a6babdbf8b
GODSPE1 <godspy18@gmail.com>
2024-04-02 07:55:37 -0700

    correct syntax

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,7 +11,7 @@ import sys
 if  __name__ == "__main__":
     """to be executable when imported"""
 
-data = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(data) as response:
-    data_response = response.getheader('X-Request-Id')
-    print(data_response)
+    data = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(data) as response:
+        data_response = response.getheader('X-Request-Id')
+        print(data_response)
