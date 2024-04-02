60e9df669b64d1867df7537f9885dc3f0d284103
GODSPE1 <godspy18@gmail.com>
2024-04-02 07:53:52 -0700

    correct to be executed when imported with comments

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,10 +8,10 @@ the header of the response.
 import urllib.request
 import sys
 
+if  __name__ == "__main__":
+    """to be executable when imported"""
+
 data = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(data) as response:
     data_response = response.getheader('X-Request-Id')
     print(data_response)
-
-if __name__ == '__main__':
-    pass
