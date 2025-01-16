65f097b63711577f70a90aada5f078e25e947b60
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2025-01-16 07:24:57 +0100

    Corrected function for power base as the absolute value will help to get the reciprocal of the value

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    a = abs(a)
     b = abs(b)
-
+    
     for i in range(b):
             result *= a
-    if a < 0:
-        return (result * -1)
-    elif b < 0:
         return (1 / (result * -1))
     else:
         return result
