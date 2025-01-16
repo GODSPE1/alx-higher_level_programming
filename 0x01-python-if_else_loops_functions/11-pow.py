2e3d34a5075490e33f835acde35eef7e733a8ab3
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2025-01-16 07:27:27 +0100

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    b = abs(b)
+
+    for i in range(abs(b)):
+        result *= a
     
-    for i in range(b):
-            result *= a
-        return (1 / (result * -1))
-    else:
-        return result
+    if b < 0:
+        return 1 / result
+    return result
